56fdbf279fdb4a5679885dad65fca84a18b33b0e
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-08-25 00:06:31 -0400

    mapped out api.js file to handle api calls and other features

---
+++ src/api.js
@@ -0,0 +1,20 @@
+// Search functionality
+
+// Capture input from the search bar
+// API call to fetch weather data based on location input
+// Handle responses, error and update UI
+
+
+// Temperature toggle
+
+// Listen for button toggle
+// Toggle active class
+// Convert temperature data between F to C
+// Update displayed temperature in the UI through calculation (not new API call)
+
+
+// Possibly cache data to prevent constant API calls
+// Loading indicator when call happens
+
+
+// Error handling
