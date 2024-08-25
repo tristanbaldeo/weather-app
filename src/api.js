945c5ecac3fd2c4cae0b95b818bfefe0ed4ec70e
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-08-25 19:47:17 -0400

    defined weatherData object to store data from api calls

--- src/api.js
+++ src/api.js
@@ -1,4 +1,28 @@
 // Search functionality
+const weatherData = {
+    city: '',
+    state: '',
+    country: '',
+    temperature: {
+        currentF: 0,
+        currentC: 0,
+        highF: 0,
+        highC: 0,
+        lowF: 0,
+        lowC: 0,
+        feelsLikeF: 0,
+        feelsLikeC: 0
+    },
+    conditions: '',
+    wind: {
+        speedMph: 0,
+        speedKph: 0,
+        direction: ''
+    },
+    humidity: 0,
+    chanceOfRain: 0,
+    lastUpdated: ''
+};
 
 // Capture input from the search bar
 // API call to fetch weather data based on location input
