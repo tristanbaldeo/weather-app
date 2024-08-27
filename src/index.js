73a6d330ab05d480595ec9833c6e8c904b2cae48
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-08-26 23:42:16 -0400

    added search function to index file for web page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
-import { createWeatherApp } from './dom.js';
+import {createWeatherApp} from './dom.js';
+import {initializeSearch} from './api.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     createWeatherApp();
+    initializeSearch();
 });
